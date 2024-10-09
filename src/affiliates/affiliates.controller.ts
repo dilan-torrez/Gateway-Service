3882f935b2402ab1b850fcd49eb84a97d828190a
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-09 19:40:02 -0400

    Avance Ruta para crear y subir documento del afiliado

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { NATS_SERVICE } from 'src/config';
 
@@ -12,6 +13,23 @@ export class AffiliatesController {
   @ApiResponse({ status: 200, description: 'Mostrar un afiliado' })
   async findOneAffiliates(@Param('id') id: string) {
     return this.client.send('affiliate.findOne', { id });
+  }
+
+  @Post(':affiliate_id/:procedure_document_id/create-document')
+  @ApiResponse({ status: 200, description: 'Enlazar y Subir Documento del Afiliado' })
+  @UseInterceptors(FileInterceptor('file_document_pdf'))
+  async createDocuments(
+    @Param('affiliate_id') affiliateId: string,
+    @Param('procedure_document_id') procedureDocumentId: string,
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    console.log('Archivo recibido:', file);
+    return this.client.send('affiliate.createDocuments', {
+      affiliateId,
+      procedureDocumentId,
+      filename: file.originalname,
+      buffer: file.buffer
+    });
   }
 
   @Get(':id/documents')
