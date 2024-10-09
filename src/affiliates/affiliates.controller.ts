7103fbf68c59f525628bc8f5c7d0d05725b000fa
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-09 06:10:40 -0400

    Ajuste end point del gateway para obtener los documentos del afiliado

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
 
 @ApiTags('affiliates')
@@ -14,10 +13,11 @@ export class AffiliatesController {
   async findOneAffiliates(@Param('id') id: string) {
     return this.client.send('affiliate.findOne', { id });
   }
-  
-  @Get(':id/show-documents')
+
+  @Get(':id/documents')
   @ApiResponse({ status: 200, description: 'Mostrar Documentos del Afiliado' })
   async showDocuments(@Param('id') id: string) {
-    return this.client.send('affiliate.showDocuments', { id });
+    return this.client.send('affiliateDocuments.showDocuments', { id });
   }
 }
+
