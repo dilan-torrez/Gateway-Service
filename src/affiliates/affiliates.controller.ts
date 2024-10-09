29b5be9e6dc59d4cf7cf7aff6ba8d44d44036c35
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-08 20:22:20 -0400

    Avance de End Point en Microservicio para obtener los documentos del Afiliate

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
 
 @ApiTags('affiliates')
 @Controller('affiliates')
 export class AffiliatesController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
-
+  
   @Get(':id')
   @ApiResponse({ status: 200, description: 'Mostrar un afiliado' })
   async findOneAffiliates(@Param('id') id: string) {
     return this.client.send('affiliate.findOne', { id });
+  }
+  
+  @Get(':id/show-documents')
+  @ApiResponse({ status: 200, description: 'Mostrar Documentos del Afiliado' })
+  async showDocuments(@Param('id') id: string) {
+    return this.client.send('affiliate.showDocuments', { id });
   }
 }
