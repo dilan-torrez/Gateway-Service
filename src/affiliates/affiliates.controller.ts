647700328619b72e7e5e389fa3a3e454b5680509
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-10 11:03:22 -0400

    End Ponit, para Enlazar y subir doucumento del afiliado

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Inject, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Inject, Param, Post, UploadedFile, UseInterceptors, UsePipes } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { NATS_SERVICE } from 'src/config';
-
+import { FileRequiredPipe } from './pipes/file-required.pipe';
 @ApiTags('affiliates')
 @Controller('affiliates')
 export class AffiliatesController {
@@ -14,21 +14,41 @@ export class AffiliatesController {
   async findOneAffiliates(@Param('id') id: string) {
     return this.client.send('affiliate.findOne', { id });
   }
-
+  
   @Post(':affiliate_id/:procedure_document_id/create-document')
-  @ApiResponse({ status: 200, description: 'Enlazar y Subir Documento del Afiliado' })
-  @UseInterceptors(FileInterceptor('file_document_pdf'))
+  @ApiOperation({ summary: 'Enlazar y Subir Documento del Afiliado' })
+  @ApiResponse({ status: 200, description: 'El documento fue subido exitosamente.' })
+  @ApiResponse({ status: 400, description: 'El archivo PDF es obligatorio o el archivo no es válido.' })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
+  @ApiParam({ name: 'affiliate_id', description: 'ID del afiliado', type: String, example: '123' })
+  @ApiParam({ name: 'procedure_document_id', description: 'ID del del documento', type: String, example: '456' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Archivo PDF del documento del afiliado',
+    type: 'multipart/form-data',
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        document_pdf: {
+          type: 'string',
+          format: 'binary',
+          description: 'Archivo PDF a subir',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('document_pdf'))
+  @UsePipes(new FileRequiredPipe())
   async createDocuments(
     @Param('affiliate_id') affiliateId: string,
     @Param('procedure_document_id') procedureDocumentId: string,
-    @UploadedFile() file: Express.Multer.File
+    @UploadedFile() document_pdf: Express.Multer.File
   ) {
-    console.log('Archivo recibido:', file);
     return this.client.send('affiliate.createDocuments', {
       affiliateId,
       procedureDocumentId,
-      filename: file.originalname,
-      buffer: file.buffer
+      buffer: document_pdf.buffer,
     });
   }
 
