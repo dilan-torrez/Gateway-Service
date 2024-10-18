d20cfb4ac073e5e80d056244fd9e946b75acb532
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-18 18:00:14 -0400

    Api para mostrar datos del afiliados y ajuste de formato

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,27 +1,51 @@
-import { Controller, Get, Inject, Param, Post, UploadedFile, UseInterceptors, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { NATS_SERVICE } from 'src/config';
 import { FileRequiredPipe } from './pipes/file-required.pipe';
 @ApiTags('affiliates')
 @Controller('affiliates')
 export class AffiliatesController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
-  
+
   @Get(':id')
-  @ApiResponse({ status: 200, description: 'Mostrar un afiliado' })
-  async findOneAffiliates(@Param('id') id: string) {
-    return this.client.send('affiliate.findOne', { id });
+  @ApiResponse({ status: 200, description: 'Mostrar datos del afiliado' })
+  async findOneData(@Param('id') id: string) {
+    return this.client.send('affiliate.findOneData', { id });
   }
-  
+
   @Post(':affiliate_id/:procedure_document_id/create-document')
   @ApiOperation({ summary: 'Enlazar y Subir Documento del Afiliado' })
   @ApiResponse({ status: 200, description: 'El documento fue subido exitosamente.' })
-  @ApiResponse({ status: 400, description: 'El archivo PDF es obligatorio o el archivo no es válido.' })
+  @ApiResponse({
+    status: 400,
+    description: 'El archivo PDF es obligatorio o el archivo no es válido.',
+  })
   @ApiResponse({ status: 500, description: 'Error interno del servidor.' })
   @ApiParam({ name: 'affiliate_id', description: 'ID del afiliado', type: Number, example: 123 })
-  @ApiParam({ name: 'procedure_document_id', description: 'ID del del documento', type: Number, example: 456 })
+  @ApiParam({
+    name: 'procedure_document_id',
+    description: 'ID del del documento',
+    type: Number,
+    example: 456,
+  })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     description: 'Archivo PDF del documento del afiliado',
@@ -43,7 +67,7 @@ export class AffiliatesController {
   async createDocuments(
     @Param('affiliate_id') affiliateId: string,
     @Param('procedure_document_id') procedureDocumentId: string,
-    @UploadedFile() document_pdf: Express.Multer.File
+    @UploadedFile() document_pdf: Express.Multer.File,
   ) {
     return this.client.send('affiliate.createDocuments', {
       affiliateId,
@@ -60,8 +84,10 @@ export class AffiliatesController {
 
   @Get(':affiliate_id/documents/:procedure_document_id')
   @ApiResponse({ status: 200, description: 'Buscar el documento del Afiliado' })
-  async findDocument(@Param('affiliate_id') affiliate_id: string, @Param('procedure_document_id') procedure_document_id: string) {
+  async findDocument(
+    @Param('affiliate_id') affiliate_id: string,
+    @Param('procedure_document_id') procedure_document_id: string,
+  ) {
     return this.client.send('affiliate.findDocument', { affiliate_id, procedure_document_id });
   }
 }
-
