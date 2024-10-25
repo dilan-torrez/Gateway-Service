7ee8b4cabcefc69ec54ec3b91505de7ce6978c21
gongor7 <crisgongor7@gmail.com>
2024-10-25 16:31:20 -0400

    cambio de nombre de DTO para filtrar personas

--- src/persons/dto/filter-person.dto.ts
+++ src/persons/dto/filter-person.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsOptional, IsString } from 'class-validator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
-export class FilterDto extends PartialType(PaginationDto) {
+export class FilteredPaginationDto extends PartialType(PaginationDto) {
   @IsString()
   @IsOptional()
   filter: string;
