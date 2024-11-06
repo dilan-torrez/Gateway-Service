c94f1218eccdeb1a5d558c2f807f0548789282ee
Adrian-Herrera-96 <adrirha@gmail.com>
2024-11-06 08:48:02 -0400

    Origin del Cors mediante .env

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  // app.enableCors({
-  //   origin: ['http://localhost:3002'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Añadir OPTIONS
-  //   allowedHeaders: 'Content-Type, Authorization',
-  //   credentials: true, // Si estás utilizando cookies o encabezados de autenticación
-  // });
+  app.enableCors({
+    origin: envs.frontendServers,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Añadir OPTIONS
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Si estás utilizando cookies o encabezados de autenticación
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
