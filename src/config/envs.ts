0b88c0ef15d6942e137c57369ca001d34d384ab8
Adrian-Herrera-96 <adrirha@gmail.com>
2024-11-06 13:42:57 -0400

    Implementar frontends servers en envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,30 +5,34 @@ interface EnvVars {
   PORT: number;
 
   NATS_SERVERS: string[];
+
+  FRONTENDS_SERVERS: string[];
 }
 
-const envsSchema = joi.object({
-  PORT: joi.number().required(),
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
 
-  NATS_SERVERS: joi.array().items( joi.string() ).required(),
-})
-.unknown(true);
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+    FRONTENDS_SERVERS: joi.array().items(joi.string()).required(),
+  })
+  .unknown(true);
 
-const { error, value } = envsSchema.validate({ 
+const { error, value } = envsSchema.validate({
   ...process.env,
-  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+  FRONTENDS_SERVERS: process.env.FRONTENDS_SERVERS?.split(','),
 });
 
-
-if ( error ) {
-  throw new Error(`Config validation error: ${ error.message }`);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars:EnvVars = value;
-
+const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
 
   natsServers: envVars.NATS_SERVERS,
-}
+  frontendServers: envVars.FRONTENDS_SERVERS,
+};
