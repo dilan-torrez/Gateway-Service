efef45bf5f9945b31135efe72cc49849d0572cad
gongor7 <crisgongor7@gmail.com>
2024-11-28 14:39:08 -0400

    Endpoint para guardar los datos de la autenticación del kiosko

--- src/kiosk/kiosk.controller.ts
+++ src/kiosk/kiosk.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpException, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { catchError } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
+import { SaveDataKioskAuthDto } from './dto/save-data-kiosk-auth.dto';
 
 @Controller('kiosk')
 @ApiTags('kiosk')
@@ -19,6 +20,15 @@ export class KioskController {
   })
   async showListFingerprint(@Param('identityCard') identityCard: string) {
     return this.client.send('kiosk.getDataPerson', identityCard).pipe(
+      catchError((err) => {
+        throw new HttpException(err, err.statusCode);
+      }),
+    );
+  }
+  @Post('saveDataKioskAuth')
+  @ApiBody({ type: SaveDataKioskAuthDto })
+  async saveDataKioskAuth(@Body() data: SaveDataKioskAuthDto) {
+    return this.client.send('kiosk.saveDataKioskAuth', data).pipe(
       catchError((err) => {
         throw new HttpException(err, err.statusCode);
       }),
