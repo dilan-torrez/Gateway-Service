fb7c5d38e79258df803e866f988e9e219ecda5dc
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-06 11:05:16 -0400

    no usados

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PersonsController } from './persons.controller';
-import { NatsModule } from '../transports/nats.module';
 
 @Module({
   controllers: [PersonsController],
   providers: [],
-  imports: [NatsModule],
+  imports: [],
 })
 export class PersonsModule {}
