1244c75259b392891d000f2fd2c3b73eb9f3f7c2
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-06 11:03:15 -0400

    ajuste de servicio nats para el modulo entidades financieras

--- src/general/financial-entities.controller.ts
+++ src/general/financial-entities.controller.ts
@@ -1,23 +1,21 @@
-import { Controller, Get, Param, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
-import { PaginationDto } from 'src/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config';
+import { PaginationDto, NatsService } from 'src/common';
 
 @ApiTags('FinancialEntity')
 @Controller('financialEntities')
 export class FinancialEntitiesController {
-  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  constructor(private readonly nats: NatsService) {}
 
   @Get()
   @ApiResponse({ status: 200, description: 'Obtener todos las entidades financieras' })
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.client.send('financialEntities.findAll', paginationDto);
+    return this.nats.send('financialEntities.findAll', paginationDto);
   }
 
   @Get(':id')
   @ApiResponse({ status: 200, description: 'Obtener una entidad financiera' })
   async findOne(@Param('id') id: string) {
-    return this.client.send('financialEntities.findOne', { id });
+    return this.nats.send('financialEntities.findOne', { id });
   }
 }
