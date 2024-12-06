5d41b6dbefb14b61a65f1b8acec5436df6b1b5c3
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-06 11:03:50 -0400

    ajuste de servicio nats para el modulo entidades de pensión

--- src/general/pension-entities.controller.ts
+++ src/general/pension-entities.controller.ts
@@ -1,23 +1,21 @@
-import { Controller, Get, Param, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
-import { PaginationDto } from 'src/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config';
+import { PaginationDto, NatsService } from 'src/common';
 
 @ApiTags('PensionEntity')
 @Controller('pensionEntities')
 export class PensionEntitiesController {
-  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  constructor(private readonly nats: NatsService) {}
 
   @Get()
   @ApiResponse({ status: 200, description: 'Obtener todos las entidades de pension' })
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.client.send('pensionEntities.findAll', paginationDto);
+    return this.nats.send('pensionEntities.findAll', paginationDto);
   }
 
   @Get(':id')
   @ApiResponse({ status: 200, description: 'Obtener una entidad de pension' })
   async findOne(@Param('id') id: string) {
-    return this.client.send('pensionEntities.findOne', { id });
+    return this.nats.send('pensionEntities.findOne', { id });
   }
 }
