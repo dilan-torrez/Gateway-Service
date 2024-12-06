461e8f3d26beb21493d2e92c66d30370156266ba
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-06 11:02:50 -0400

    ajuste de servicio nats para el modulo categorias

--- src/general/categories.controller.ts
+++ src/general/categories.controller.ts
@@ -1,23 +1,21 @@
-import { Controller, Get, Param, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
-import { PaginationDto } from 'src/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config';
+import { PaginationDto, NatsService } from 'src/common';
 
 @ApiTags('Category')
 @Controller('categories')
 export class CategoriesController {
-  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  constructor(private readonly nats: NatsService) {}
 
   @Get()
   @ApiResponse({ status: 200, description: 'Obtener todos las categorías' })
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.client.send('categories.findAll', paginationDto);
+    return this.nats.send('categories.findAll', paginationDto);
   }
 
   @Get(':id')
   @ApiResponse({ status: 200, description: 'Obtener una categoría' })
   async findOne(@Param('id') id: string) {
-    return this.client.send('categories.findOne', { id });
+    return this.nats.send('categories.findOne', { id });
   }
 }
