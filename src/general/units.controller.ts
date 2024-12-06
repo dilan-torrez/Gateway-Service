818df67f95ef98e4389a7cedf21d16d4e015cc79
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-06 11:04:18 -0400

    ajuste de servicio nats para el modulo unidades

--- src/general/units.controller.ts
+++ src/general/units.controller.ts
@@ -1,35 +1,33 @@
-import { Controller, Get, Param, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
-import { PaginationDto } from 'src/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config';
+import { PaginationDto, NatsService } from 'src/common';
 
 @ApiTags('Unit')
 @Controller()
 export class UnitsController {
-  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  constructor(private readonly nats: NatsService) {}
 
   @Get('units')
   @ApiResponse({ status: 200, description: 'Obtener todas las unidades' })
   findAllUnits(@Query() paginationDto: PaginationDto) {
-    return this.client.send('units.findAll', paginationDto);
+    return this.nats.send('units.findAll', paginationDto);
   }
 
   @Get('units/:id')
   @ApiResponse({ status: 200, description: 'Obtener una unidad' })
   async findOneUnits(@Param('id') id: string) {
-    return this.client.send('units.findOne', { id });
+    return this.nats.send('units.findOne', { id });
   }
 
   @Get('breakdowns')
   @ApiResponse({ status: 200, description: 'Obtener todos los estados' })
   findAllBreakdowns(@Query() paginationDto: PaginationDto) {
-    return this.client.send('breakdowns.findAll', paginationDto);
+    return this.nats.send('breakdowns.findAll', paginationDto);
   }
 
   @Get('breakdowns/:id')
   @ApiResponse({ status: 200, description: 'Obtener un estado' })
   async findOneBreakdowns(@Param('id') id: string) {
-    return this.client.send('breakdowns.findOne', { id });
+    return this.nats.send('breakdowns.findOne', { id });
   }
 }
