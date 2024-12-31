cd4886449adb2b4d4854e70f1184095cdd919366
ferbautistamollo <ferbautistamollo@gmail.com>
2024-12-31 08:47:09 -0400

    ajuste main cors documentos

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
     origin: FrontEnvs.frontendServers,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Añadir OPTIONS
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization, Credentials',
     credentials: true, // Si estás utilizando cookies o encabezados de autenticación
   });
   app.useGlobalPipes(
