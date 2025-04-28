06907fca13e889888755b45dd03fe5e9daf64f21
gongor7 <crisgongor7@gmail.com>
2025-04-28 12:09:39 -0400

    Interfaz para validar al request con el usuario autenticado

---
+++ src/auth/interfaces/authenticated-request.interface.ts
@@ -0,0 +1,7 @@
+// src/common/types/authenticated-request.interface.ts
+import { Request } from 'express';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+
+export interface AuthenticatedRequest extends Request {
+  user: JwtPayload;
+}
