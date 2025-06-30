17139f7a5d3310a41adf9aaebf1ace3d2a1b2ee6
ferbautistamollo <ferbautistamollo@gmail.com>
2025-06-30 16:25:49 -0400

    CREANDO VALIDACIÓN PARA CHUNKS

--- src/common/pipes/file-required.pipe.ts
+++ src/common/pipes/file-required.pipe.ts
@@ -6,10 +6,24 @@ export class FileRequiredPipe implements PipeTransform {
     if (!file) {
       throw new BadRequestException('El archivo PDF es obligatorio');
     }
-    console.log('validación');
 
     if (file.mimetype !== 'application/pdf') {
       throw new BadRequestException('El archivo debe ser PDF');
+    }
+
+    return file;
+  }
+}
+
+@Injectable()
+export class FileChunkRequiredPipe implements PipeTransform {
+  transform(file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('El archivo es obligatorio');
+    }
+
+    if (file.mimetype !== 'application/octet-stream') {
+      throw new BadRequestException('El archivo debe ser binario - chunk');
     }
 
     return file;
