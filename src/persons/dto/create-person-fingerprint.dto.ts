8ff4346c6d8534584551526b060d4eb64b0b1347
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-14 14:01:19 -0400

    REFACTORIZACIÓN DTOP CREAR HUELLAS

--- src/persons/dto/create-person-fingerprint.dto.ts
+++ src/persons/dto/create-person-fingerprint.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsString, IsNotEmpty, IsArray, ValidateNested } from 'class-validator';
+import { IsInt, IsNotEmpty, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
@@ -8,29 +8,32 @@ class FingerprintData {
   @IsNotEmpty()
   fingerprintTypeId: number;
 
-  @ApiProperty({ description: 'Huella digital en formato WSQ', example: '...' })
-  @IsNotEmpty()
-  @IsString()
-  wsq: string;
-
   @ApiProperty({ description: 'Calidad de la huella', example: 80 })
   @IsInt()
   @IsNotEmpty()
   quality: number;
 }
 
-export class CreatePersonFingerprintDto {
-  @ApiProperty({ description: 'ID de la persona', example: 1243 })
-  @IsInt()
-  @IsNotEmpty()
-  personId: number;
+class WsqFingerprintDto {
+  [key: string]: string;
+}
 
+export class CreatePersonFingerprintDto {
   @ApiProperty({
-    type: [FingerprintData], // Aquí se especifica que es un array de FingerprintData
+    type: [FingerprintData],
     description: 'Lista de huellas digitales',
   })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => FingerprintData)
-  fingerprints: FingerprintData[];
+  personFingerprints: FingerprintData[];
+
+  @ApiProperty({
+    type: [WsqFingerprintDto],
+    description: 'Lista de huellas digitales en formato WSQ',
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => WsqFingerprintDto)
+  wsqFingerprints: WsqFingerprintDto[];
 }
