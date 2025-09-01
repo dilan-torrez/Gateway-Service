3f92cad8f1c7b7f3e8c4f9b0f4c7337d1ec6ea57
ferbautistamollo <ferbautistamollo@gmail.com>
2025-09-01 08:59:38 -0400

    SERVICIO NATS PARA EMITIR FUNCION A EJECUTAR

--- src/common/nats/nats.service.ts
+++ src/common/nats/nats.service.ts
@@ -23,4 +23,14 @@ export class NatsService {
   async firstValue(service: string, data: any): Promise<any> {
     return firstValueFrom(await this.send(service, data));
   }
+
+  async emit(service: string, data: any): Promise<void> {
+    try {
+      this.client.emit(service, data);
+      this.logger.log(`Event emitted to [${service}] with data: ${JSON.stringify(data)}`);
+    } catch (error) {
+      this.logger.error(`Failed to emit event to [${service}]`, error.stack);
+      throw new HttpException('Event emit failed', 500);
+    }
+  }
 }
