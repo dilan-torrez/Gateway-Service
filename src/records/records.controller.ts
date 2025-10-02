65618fe8a44483f92647fbe3af0d68ad65b93156
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-02 16:40:53 -0400

    CREACION DE ARCHIVOS DE FUNCIONAMIENTO DEL MODULO RECORDS

---
+++ src/records/records.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { NatsService } from 'src/common';
+
+@ApiTags('records')
+@Controller('records')
+export class RecordsController {
+  constructor(private readonly nats: NatsService) {}
+
+  @Get('appMobile/:affiliateId')
+  async findAllAppMobile(@Param('affiliateId') affiliateId: string) {
+    console.log('affiliateId', affiliateId);
+    return this.nats.firstValue('records.findAllAppMobile', { affiliateId });
+  }
+
+  @Get('beneficiary/:affiliateId')
+  async findAllBeneficiary(@Param('affiliateId') affiliateId: string) {
+    console.log('affiliateId', affiliateId);
+    return this.nats.firstValue('records.findAll', {});
+  }
+}
