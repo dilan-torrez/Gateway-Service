2993ef63403ae5133df14940a4cf0b50518f0d61
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-02 16:36:39 -0400

    AJUSTE DE ARCHIVOS Y ELIMICACIÓN DE RUTAS DE TABLAS ESTATICAS

--- src/persons/dto/index.ts
+++
@@ -1,4 +0,0 @@
-export { CreatePersonDto } from './create-person.dto';
-export { UpdatePersonDto } from './update-person.dto';
-export { FilteredPaginationDto } from './filter-person.dto';
-export { CreatePersonFingerprintDto } from './create-person-fingerprint.dto';
